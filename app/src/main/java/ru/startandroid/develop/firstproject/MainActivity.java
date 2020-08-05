74316834f09287eacabfde0e992e35379279a8d2
yurasika25 <yrasika80@gmail.com>
2020-08-05 15:41:02 +0300

    message 2

--- app/src/main/java/ru/startandroid/develop/firstproject/MainActivity.java
+++ app/src/main/java/ru/startandroid/develop/firstproject/MainActivity.java
@@ -3,6 +3,7 @@ package ru.startandroid.develop.firstproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FilterQueryProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,8 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         takeKeyEvents(databaseList());
         unbindService();
+        FilterQueryProvider (DEFAULT_KEYS_DIALER)
     }
 }
